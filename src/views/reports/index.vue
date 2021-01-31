e589876fba143c707c58f00fef502eedf2cf089b
wenhuan <823302432@qq.com>
2021-01-31 21:02:39 +0800

    '完成了报表功能'

--- src/views/reports/index.vue
+++ src/views/reports/index.vue
@@ -1,19 +1,71 @@
 <template>
-  <div class="reports-container">统计报表</div>
+  <div class="reports-container">
+    <div ref="main" style="width: 750px; height: 400px"></div>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import { getReport } from '@/api/orders'
+import _ from 'lodash'
+
 export default {
   name: 'reportsIndex',
   components: {},
   props: {},
   data() {
-    return {}
+    return {
+      // 需要合并的对象
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
   },
   computed: {},
   watch: {},
   created() {},
-  mounted() {},
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(this.$refs.main)
+    //   获取报表数据
+    const { data } = await getReport()
+    if (data.meta.status === 200) {
+      this.$message.success('获取折线图成功')
+    } else {
+      this.$message.error('获取失败')
+    }
+    // 合并获取的数据
+    const result = _.merge(data.data, this.options)
+
+    myChart.setOption(result)
+  },
   methods: {}
 }
 </script>
